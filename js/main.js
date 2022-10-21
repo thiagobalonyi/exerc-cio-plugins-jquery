2d781d3ef9630b0866572fcb3f2fc2cd3602ed84
Thiago Balonyi <thiagocandal@gmail.com>
2022-10-21 11:31:41 -0300

    Commit Inicial

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+});
+
+$('#telefone').mask('(00) 00000-0000');
+
+$('#cpf').mask('000.000.000-00');
+
+$('#cep').mask('00000-000')
+
+$('form').validate ({
+    rules: {
+        nome: {
+            required: true
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        cpf: {
+            required: true
+        },
+        endereço: {
+            required: true
+        },
+        cep: {
+            required: true
+        }
+    },
+    messages: {
+        nome: 'Por favor, insira seu nome'
+    },
+    submitHandler: function(form){
+        console.log(form)
+    },
+    invalidHandler: function(evento, validador) {
+        let camposIncorretos = validador.numberOfInvalids();
+        if (camposIncorretos) {
+            alert(`Existem ${camposIncorretos} campos obrigatórios`)
+        }
+    }
+});
